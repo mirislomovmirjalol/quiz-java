992f5d008b6e31256c9d9e98aee9c16267b70667
mirjalol <mirislomovmirjalol@gmail.com>
2023-03-22 09:41:54 +0000

    data path unit test

---
+++ src/Tests/DataPathTest.java
@@ -0,0 +1,27 @@
+package Tests;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class DataPathTest {
+    @org.junit.jupiter.api.Test
+    void usersData() throws FileNotFoundException {
+        final String usersPath = "src/Data/Database/Users.csv";
+        java.io.File file = new java.io.File(usersPath);
+        FileReader fileReader = new FileReader(file);
+    }
+
+    @org.junit.jupiter.api.Test
+    void questionsData() throws FileNotFoundException {
+        final String questionsPath = "src/Data/Database/Questions.csv";
+        java.io.File file = new java.io.File(questionsPath);
+        FileReader fileReader = new FileReader(file);
+    }
+
+    @org.junit.jupiter.api.Test
+    void quizData() throws FileNotFoundException {
+        final String quizPath = "src/Data/Database/Quizzes.csv";
+        java.io.File file = new java.io.File(quizPath);
+        FileReader fileReader = new FileReader(file);
+    }
+}
