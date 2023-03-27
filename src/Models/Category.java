c735d9244551b261a01a7b25daa7212c3094fc02
mirjalol <mirislomovmirjalol@gmail.com>
2023-03-27 13:34:34 +0100

    category model and category menu for admin

---
+++ src/Models/Category.java
@@ -0,0 +1,48 @@
+package Models;
+
+import java.util.ArrayList;
+
+public class Category {
+    private int id;
+    private String title;
+    private String description;
+    private ArrayList<Question> questions;
+
+    public Category(int id, String title, String description) {
+        setId(id);
+        setName(title);
+        setDescription(description);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return title;
+    }
+
+    public void setName(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(ArrayList<Question> questions) {
+        this.questions = questions;
+    }
+}
---
+++ src/Views/Admin/CategoriesMenu.java
@@ -0,0 +1,112 @@
+package Views.Admin;
+
+import Controllers.Admin.CategoriesController;
+import Controllers.AuthController;
+import Models.Category;
+import Views.KeyboardReader;
+import Views.Messenger;
+
+import java.util.ArrayList;
+
+public class CategoriesMenu {
+    AuthController authController;
+    ArrayList<Category> categories = new ArrayList<Category>();
+    Messenger messenger = new Messenger();
+    KeyboardReader keyboardReader = new KeyboardReader();
+    CategoriesController categoriesController;
+
+    public CategoriesMenu(AuthController authController) {
+        this.authController = authController;
+        this.categoriesController = new CategoriesController(authController);
+    }
+
+    public void run() {
+        if (!authController.isUserAdmin()) {
+            messenger.notAdmin();
+            return;
+        }
+        final int SHOW_CATEGORIES = 1;
+        final int CREATE_CATEGORY = 2;
+        final int UPDATE_CATEGORY = 3;
+        final int DELETE_CATEGORY = 4;
+        final int BACK_OPTION = 5;
+        messenger.oneLineTitle("Categories");
+        String[] options = {"Show", "Create", "Update", "Delete", "Back"};
+        boolean exit = false;
+
+
+        while (!exit) {
+            for (int i = 0; i < options.length; i++) {
+                System.out.println((i + 1) + ". " + options[i]);
+            }
+            int choice = keyboardReader.getInt("\n\nPlease choose an option", 0, options.length);
+            switch (choice) {
+                case SHOW_CATEGORIES -> categoriesController.showCategories(false);
+                case CREATE_CATEGORY -> create();
+                case UPDATE_CATEGORY -> update();
+                case DELETE_CATEGORY -> delete();
+                case BACK_OPTION -> exit = true;
+            }
+        }
+    }
+
+    public void showCategories(ArrayList<Category> categories, boolean isAction) {
+        for (Category category : categories) {
+            System.out.println(category.getId() + ". " + category.getName() + " | " + category.getDescription());
+        }
+        if (!isAction) {
+            keyboardReader.getString("Press enter to continue", true);
+        }
+    }
+
+    public void create() {
+        messenger.oneLineTitle("Please enter title for new category");
+        String title = keyboardReader.getString("Title", false);
+        messenger.oneLineTitle("Please enter description for new category");
+        String description = keyboardReader.getString("Description", false);
+        categoriesController.create(title, description);
+    }
+
+    public void update() {
+        categoriesController.showCategories(true);
+        messenger.oneLineTitle("Please enter id of category to update");
+        int id = keyboardReader.getInt("Id", 0, Integer.MAX_VALUE);
+        Category category = categoriesController.getCategory(id);
+        if (category == null) {
+            messenger.notFound("Category");
+            return;
+        }
+        final int UPDATE_TITLE = 1;
+        final int UPDATE_DESCRIPTION = 2;
+        boolean exit = false;
+        while (!exit) {
+            messenger.oneLineTitle("What do you want to update?");
+            String[] options = {"Title", "Description", "Back"};
+            for (int i = 0; i < options.length; i++) {
+                System.out.println((i + 1) + ". " + options[i]);
+            }
+            int choice = keyboardReader.getInt("\n\nPlease choose an option", 0, options.length);
+            switch (choice) {
+                case UPDATE_TITLE -> updateTitle(category);
+                case UPDATE_DESCRIPTION -> updateDescription(category);
+            }
+        }
+    }
+
+    public void delete() {
+        System.out.println("delete");
+    }
+
+    public void updateTitle(Category category) {
+        messenger.oneLineTitle("Please enter new title for category");
+        String title = keyboardReader.getString("Title", false);
+        category.setName(title);
+    }
+
+    public void updateDescription(Category category) {
+        messenger.oneLineTitle("Please enter new description for category");
+        String description = keyboardReader.getString("Description", false);
+        category.setDescription(description);
+    }
+
+}
